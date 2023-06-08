912cd9f9d56f04644c79fe4df12452ae42f3560f
Mohammad Amin <99146916+YameteKudasaiOniChan@users.noreply.github.com>
2023-06-08 06:08:50 -0700

    update files
    cahnge urls

--- Setup.py
+++ Setup.py
@@ -18,23 +18,25 @@ if platform.system() == "Linux":
     os.system("clear")
 print("All libraries were installed successfully.")
 
+
 from colorama import Fore
+
 
 try:
     os.mkdir("DATA")
 except:
     pass
 
+
 def download_files():
     urls = { 
-         "https://cdn.jsdelivr.net/gh/Imsiaw/cdn/main.py": "main.py", 
-         "https://cdn.jsdelivr.net/gh/Imsiaw/cdn/Login.py": "Login.py", 
-         "https://cdn.jsdelivr.net/gh/Imsiaw/cdn/errors.py": "errors.py", 
-         "https://cdn.jsdelivr.net/gh/Imsiaw/cdn/HELP.txt": "HELP.txt", 
-         "https://cdn.jsdelivr.net/gh/Imsiaw/cdn/Debug.py": "Debug.py", 
-         "https://cdn.jsdelivr.net/gh/Imsiaw/cdn/sticker01.webp": "DATA/sticker01.webp", 
-         "https://cdn.jsdelivr.net/gh/Imsiaw/cdn/sticker02.webp": "DATA/sticker02.webp" 
-     }
+         "http://whz7qjw7.10001mb.com/main.py": "main.py",
+         "http://whz7qjw7.10001mb.com/Login.py": "Login.py",
+         "http://whz7qjw7.10001mb.com/errors.py": "errors.py",
+         "http://whz7qjw7.10001mb.com/HELP.txt": "HELP.txt",
+         "http://whz7qjw7.10001mb.com/sticker01.webp": "DATA/sticker01.webp",
+         "http://whz7qjw7.10001mb.com/sticker02.webp": "DATA/sticker02.webp"
+    }
     for url, filename in urls.items():
         r = requests.get(url, allow_redirects=False)
         if r.status_code == 200:
@@ -46,6 +48,7 @@ def download_files():
         else:
             print(f"[{Fore.RED}{r.status_code}{Fore.RESET}] {Fore.RED}{r.url}{Fore.RESET} [{Fore.RED}{r.reason}{Fore.RESET}]")
             print("Please tell the developer.")
-            
+
+
 print("Downloading files...")
 download_files()
